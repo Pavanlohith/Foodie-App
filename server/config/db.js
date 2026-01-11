0eaeca6efd81a5977ad313d28f985d668fd8097a
Lohith <pavanlohith006@gmail.com>
2026-01-12 00:53:39 +0530

    given env files

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://pavanlohith006_db_user:pavan12345@cluster0.ujc5y2u.mongodb.net/fooddel"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ DB connected");
   } catch (error) {
-    console.error(" DB connection failed:", error.message);
+    console.error("❌ DB connection failed:", error.message);
+    process.exit(1);
   }
 };
