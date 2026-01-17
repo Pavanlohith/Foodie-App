ecfd5deee5dd9b2b72a77bfe215e3b60fef7e501
Lohith <pavanlohith006@gmail.com>
2026-01-17 19:52:08 +0530

    backend issue

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "https://tangerine-kangaroo-e403f4.netlify.app",
